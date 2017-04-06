a6cfbaed6c44b55ef02f99aa21b4c7526c1e7323
brajendrak0068 <brajendrak0068@gmail.com>
2017-04-06 19:03:51 +0530

    move gtm script to onload event

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ module.exports = {
 
   contentFor: function(type, config) {
     var config = config['ember-cli-gtm']
-    var appId = config && config.appId
+    var appId = config && config.appId;   
+       
     if (type === 'body' && appId) {
-      return "<noscript><iframe src='//www.googletagmanager.com/ns.html?id=" + appId + "' height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript> <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','" + appId + "');</script>";
+      return "if (window.addEventListener){ window.addEventListener('load', downloadJSAtOnload, false);} else if(window.attachEvent){ window.addEventListener('load', downloadJSAtOnload, false);} else { window.onload = downloadJSAtOnload;} ;<noscript><iframe src='//www.googletagmanager.com/ns.html?id=" + appId + "' height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript> <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= '//www.googletagmanager.com/gtm.js?id='+i+dl; downloadJSAtOnload(){f.parentNode.insertBefore(j,f); }})(window,document,'script','dataLayer','" + appId + "');</script>";
     }
   }
 };
